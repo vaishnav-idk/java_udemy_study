00c65a3a6c3a1d3f52259047458edc47e1c0831c
vaishnav-idk <vaishnav_chandran@yahoo.com>
2024-11-13 19:43:11 +0530

    classes challenge question account class created and function added

---
+++ Classes_Challenge/src/Account.java
@@ -0,0 +1,48 @@
+public class Account {
+    private double balance;
+    private int  number;
+    private String customerName;
+    private String customerEmail;
+    private String customerPhone;
+
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+}
