c6742395d3160a8b10c6980efb7387f3512972e5
vaishnav-idk <vaishnav_chandran@yahoo.com>
2024-11-13 19:49:42 +0530

    Main.java done

--- Classes_Challenge/src/Main.java
+++ Classes_Challenge/src/Main.java
@@ -2,6 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Account account1 = new Account();
+        account1.withdrawFunds(100.0);
+        account1.depsoitFunds(100.0);
+        account1.withdrawFunds(55.5);
 
     }
+
 }
