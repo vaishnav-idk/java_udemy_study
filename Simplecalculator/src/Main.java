3b7ab9034fbcceb993ee83c0146b26133823c75b
vaishnav-idk <vaishnav_chandran@yahoo.com>
2024-11-13 20:00:31 +0530

    test question completed

---
+++ Simplecalculator/src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        SimpleCalculator calculator = new SimpleCalculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("add= " + calculator.getAdditionResult());
+        System.out.println("subtract= " + calculator.getSubtractionResult());
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("multiply= " + calculator.getMultiplicationResult());
+        System.out.println("divide= " + calculator.getDivisionResult());
+        }
+    }
---
+++ Simplecalculator/src/SimpleCalculator.java
@@ -0,0 +1,37 @@
+public class SimpleCalculator {
+    private double firstNumber;
+    private double secondNumber;
+
+    public double getFirstNumber() {
+        return firstNumber;
+    }
+
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public double getSecondNumber() {
+        return secondNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    public double getAdditionResult() {
+        return firstNumber + secondNumber;
+    }
+    public double getSubtractionResult() {
+        return firstNumber - secondNumber;
+    }
+    public double getMultiplicationResult() {
+        return firstNumber * secondNumber;
+    }
+    public double getDivisionResult() {
+        if (firstNumber == 0 || secondNumber == 0) {
+           return 0.0;
+        }
+        return firstNumber / secondNumber;
+    }
+
+}
