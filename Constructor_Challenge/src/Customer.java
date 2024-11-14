9e4046e47c22552dc1498cc84ebb105c94154984
vaishnav-idk <vaishnav_chandran@yahoo.com>
2024-11-14 09:36:02 +0530

    getters added , private instance variable added

---
+++ Constructor_Challenge/src/Customer.java
@@ -0,0 +1,17 @@
+public class Customer {
+    private String name;
+    private double creditLimit;
+    private  String email;
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
