7edb3c40d068b0713a2b4d0b8897e8850f31e650
vaishnav-idk <vaishnav_chandran@yahoo.com>
2024-11-16 16:09:00 +0530

    dog class contstuctor completed

--- inheritence/src/Dog.java
+++ inheritence/src/Dog.java
@@ -2,7 +2,7 @@ public class Dog extends Animal {
     private String earShape;
 
     public Dog(){
-        super();
+        super("mutt","small","50");
     }
 
 
--- inheritence/src/Main.java
+++ inheritence/src/Main.java
@@ -2,7 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+    Animal animal = new Animal("Generic Animal","Huge","400");
+    doAnimalStuff(animal,"slow");
+    Dog dog = new Dog();
+    doAnimalStuff(dog,"slow");
     }
 
     public static void doAnimalStuff(Animal animal, String speed) {
