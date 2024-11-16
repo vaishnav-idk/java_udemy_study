a1bf0d134bbaa8f384c4e16c7c4fd3fe235eb770
vaishnav-idk <vaishnav_chandran@yahoo.com>
2024-11-16 18:27:47 +0530

    Worker.java done

--- InheritenceChallenge/src/Worker.java
+++ InheritenceChallenge/src/Worker.java
@@ -20,4 +20,21 @@ public class Worker {
         return (currentYear-birthYear);
     }
 
+    public double collectPay()
+    {
+        return 0.0;
+    }
+
+    @Override
+    public String toString() {
+        return "Worker{" +
+                "name='" + name + '\'' +
+                ", birthDate='" + birthDate + '\'' +
+                ", endDate='" + endDate + '\'' +
+                '}';
+    }
+
+    public void terminate(String endDate) {
+         this.endDate = endDate;
+    }
 }
