09672adf62ea3ef9406a8c925f5972eb4573ede2
vaishnav-idk <vaishnav_chandran@yahoo.com>
2024-11-16 18:37:38 +0530

    using a private counter for employee id in Employee.java

--- InheritenceChallenge/src/Employee.java
+++ InheritenceChallenge/src/Employee.java
@@ -1,11 +1,12 @@
 public class Employee extends  Worker{
     private long employeeID;
     private String hireDate;
-
+    private static int employeeCounter=1;
+    //static variable
     public Employee(String name, String birthDate, String hireDate, long employeeID) {
         super(name, birthDate);
         this.hireDate = hireDate;
-        this.employeeID = employeeID;
+        this.employeeID =Employee.employeeCounter++;
     }
 
     @Override
