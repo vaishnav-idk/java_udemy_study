169514ac836b869992b7c3d7d90823529f13a351
vaishnav-idk <vaishnav_chandran@yahoo.com>
2024-11-17 20:15:18 +0530

    SmartKitchen.java completed

--- Composition_Challenge/src/SmartKitchen.java
+++ Composition_Challenge/src/SmartKitchen.java
@@ -1,3 +1,51 @@
 public class SmartKitchen {
+private CoffeeMaker brewMaster;
+private Refrigerator iceBox;
+private DishWasher dishWasher;
 
 }
+//only one public class in a source file
+class CoffeeMaker {
+    private boolean hasWorkToDo;
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+
+    public void brewCoffee() {
+        if (hasWorkToDo) {
+            System.out.println("Brewing Coffee");
+        }
+    }
+
+    class Refrigerator {
+        private boolean hasWorkToDo;
+
+        public void setHasWorkToDo(boolean hasWorkToDo) {
+            this.hasWorkToDo = hasWorkToDo;
+        }
+
+        public void orderFood() {
+            if (hasWorkToDo) {
+                System.out.println("Ordering food");
+            }
+        }
+
+
+    }
+    class dishWasher {
+        private boolean hasWorkToDo;
+        public void setHasWorkToDo(boolean hasWorkToDo)
+        {
+            this.hasWorkToDo=hasWorkToDo;
+        }
+
+        public void doDishs(){
+            if(hasWorkToDo)
+            {
+                System.out.println("washing");
+            }
+        }
+
+
+    }
