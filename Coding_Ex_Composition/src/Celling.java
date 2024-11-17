bdd48fc3250797a53259b5af5e747caf9edb65be
vaishnav-idk <vaishnav_chandran@yahoo.com>
2024-11-17 22:18:42 +0530

    Celling.java done

---
+++ Coding_Ex_Composition/src/Celling.java
@@ -0,0 +1,17 @@
+public class Celling {
+    private int height;
+    private int paintedColor;
+    public Celling(int height, int paintedColor) {
+        this.height = height;
+        this.paintedColor = paintedColor;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getPaintedColor() {
+        return paintedColor;
+    }
+
+}
