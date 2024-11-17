788d8171f2b3f6258e9abdfffe0eadabeb93ada8
vaishnav-idk <vaishnav_chandran@yahoo.com>
2024-11-17 18:38:11 +0530

    corrections added to classes and example completed

--- Coding_Excerise_Cylinder/src/Circle.java
+++ Coding_Excerise_Cylinder/src/Circle.java
@@ -1,8 +1,11 @@
 public class Circle
 {
-double radius;
+private double radius;
 public Circle(double radius){
     this.radius = (radius < 0) ? 0 : radius;
+}
+public double getRadius(){
+    return radius;
 }
 public double getArea(){
     return Math.PI * radius * radius;
--- Coding_Excerise_Cylinder/src/Cylinder.java
+++ Coding_Excerise_Cylinder/src/Cylinder.java
@@ -1,6 +1,17 @@
 
 public class Cylinder extends Circle{
-    public Cylinder(double radius) {
+    private double height;
+    public Cylinder(double radius,double height) {
         super(radius);
+        this.height = (height<0)?0:height;
     }
+    public double getHeight() {
+        return height;
+    }
+    public double getVolume() {
+        double area=super.getArea();
+        return area*height;
+
+    }
+
 }
