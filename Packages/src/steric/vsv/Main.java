a490f8e64ba137060ce921ef5153662a1318eb88
vaishnav-idk <vaishnav_chandran@yahoo.com>
2024-11-19 07:51:45 +0530

    Main.java finished and calls the Item function form the package

--- Packages/src/steric/vsv/Main.java
+++ Packages/src/steric/vsv/Main.java
@@ -1,4 +1,10 @@
 package steric.vsv;
 
+import com.abc.first.Item;
+
 public class Main {
+    public static void main(String[] args) {
+        Item firstItem = new Item("First Item");
+        System.out.println(firstItem);
+    }
 }
