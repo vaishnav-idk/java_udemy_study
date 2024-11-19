6ec5aed56336f6734039d20612e470abfd61b9a0
vaishnav-idk <vaishnav_chandran@yahoo.com>
2024-11-19 07:54:28 +0530

    Example using fully qualified named

--- Packages/src/steric/vsv/Main.java
+++ Packages/src/steric/vsv/Main.java
@@ -4,7 +4,10 @@ import com.abc.first.Item;
 
 public class Main {
     public static void main(String[] args) {
-        Item firstItem = new Item("First Item");
-        System.out.println(firstItem);
+       // Item firstItem = new Item("First Item");
+        //System.out.println(firstItem);
+        com.abc.first.Item thirdItem = new com.abc.first.Item("Third Item");
+        //we can use fully qualified name of class and not import the package
+        System.out.println(thirdItem);
     }
 }
