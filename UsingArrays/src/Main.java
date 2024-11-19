d68d4a25587573fa77fa14cba55c4fbae8dd1584
vaishnav-idk <vaishnav_chandran@yahoo.com>
2024-11-19 12:02:56 +0530

    array before sorting

--- UsingArrays/src/Main.java
+++ UsingArrays/src/Main.java
@@ -1,11 +1,17 @@
+import java.util.Arrays;
 import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-
+    int[] firstArray = getRandomArray(10);
+    System.out.println(Arrays.toString(firstArray));
     }
 
     public static int[] getRandomArray(int len)
     {
         int[] arr = new int[len];
+        for(int i = 0; i < len; i++){
+            arr[i] = new Random().nextInt(100);
+        }
+        return arr;
     }
 }
