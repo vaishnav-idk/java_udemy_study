7149939afac514a1ef4dde92d3003d285600afdf
vaishnav-idk <vaishnav_chandran@yahoo.com>
2025-01-09 21:19:42 +0530

    Arrays done

--- autoboxing/src/Main.java
+++ autoboxing/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -10,6 +12,14 @@ public class Main {
         Integer autoint=15;
         int autoubint=autoint;
         System.out.println(autoint.getClass().getName());
+        Integer[] wrapperarray=new Integer[10];
+        wrapperarray[0]=50;
+        wrapperarray[1]=20;
+        System.out.println(Arrays.toString(wrapperarray));
+        //Arrays.toString() factory class works for objects too
+        Character[] chararray={'a','b','c'};//autoboxing for anonmyous array
+        System.out.println(Arrays.toString(chararray));
+
         }
         public static Double getDouble() {
         return Double.valueOf(Math.random());
