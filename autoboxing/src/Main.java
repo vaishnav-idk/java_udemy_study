061a63f904b2f65e5d9b11ea7b8ce304cb95ddc3
vaishnav-idk <vaishnav_chandran@yahoo.com>
2025-01-09 21:15:32 +0530

    double also done

--- autoboxing/src/Main.java
+++ autoboxing/src/Main.java
@@ -3,7 +3,18 @@
 public class Main {
     public static void main(String[] args) {
             Integer boxedInt=Integer.valueOf(15);
-            Integer boxedInt2=new Integer(15);
+            //Integer boxedInt2=new Integer(15);
             int ubint=boxedInt.intValue();
+
+            //automatic
+        Integer autoint=15;
+        int autoubint=autoint;
+        System.out.println(autoint.getClass().getName());
+        }
+        public static Double getDouble() {
+        return Double.valueOf(Math.random());
+        }
+        private static double getDouble2() {
+        return 100.0;
         }
     }
