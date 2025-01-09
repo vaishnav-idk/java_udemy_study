4846f2fa2d188b02990794d31f23a54911ccb2c9
vaishnav-idk <vaishnav_chandran@yahoo.com>
2025-01-09 21:08:06 +0530

    intial testing

---
+++ autoboxing/src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+            Integer boxedInt=Integer.valueOf(15);
+            Integer boxedInt2=new Integer(15);
+            int ubint=boxedInt.intValue();
+        }
+    }
